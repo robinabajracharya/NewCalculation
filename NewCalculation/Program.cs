5f49c407fa882318a58e29c3371e1d4cef7090c2
Ursha Munikar <ursha.munikar12@gmail.com>
2018-09-28 09:57:46 +0545

    implemented and tested as function

--- NewCalculation/Program.cs
+++ NewCalculation/Program.cs
@@ -13,13 +13,19 @@ namespace NewCalculation
             double a = 3;
             double b = 5;
             double add = Add(a, b);
-
+            double product = Multiply(a, b);
             Console.WriteLine("sum={0}", add);
+            Console.WriteLine("product={0}", product);
         }
 
         static double Add(double a,double b)
         {
             return a + b;
         }
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
     }
 }
